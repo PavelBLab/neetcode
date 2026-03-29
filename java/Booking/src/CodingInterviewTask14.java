094ad2cd44556c78db57581b30927277baf939d5
pavelbrustinov <brustinov.pavel@gmail.com>
2026-03-29 19:34:12 +0200

    Added 14th Problem: Nearest Hotel Finder

---
+++ java/Booking/src/CodingInterviewTask14.java
@@ -0,0 +1,104 @@
+import java.util.*;
+
+public class CodingInterviewTask14 {
+
+    /*
+     * Problem: Nearest Hotel Finder
+     *
+     * Booking.com is building a feature to show travelers the nearest hotel
+     * from their current location in a city grid.
+     *
+     * You are given a 2D grid where:
+     *   0 = road (walkable)
+     *   1 = building (blocked, cannot pass through)
+     *   2 = hotel
+     *
+     * Given a starting position (startRow, startCol), find the minimum
+     * number of steps to reach the nearest hotel.
+     * You can move up, down, left, right (not diagonally).
+     * If no hotel is reachable, return -1.
+     */
+    public static void main(String[] args) {
+        // Test 1: hotel one step away
+        int[][] grid1 = {
+                {0, 0, 0, 2},
+                {0, 1, 1, 0},
+                {0, 0, 0, 0},
+                {2, 1, 0, 0}
+        };
+        System.out.println(solution1(grid1, 2, 0)); // Expected: 1
+
+        // Test 2: must navigate around walls
+        int[][] grid2 = {
+                {0, 1, 2},
+                {0, 1, 0},
+                {0, 0, 0}
+        };
+        System.out.println(solution1(grid2, 0, 0)); // Expected: 6
+
+        // Test 3: hotel unreachable
+        int[][] grid3 = {
+                {0, 1},
+                {1, 2}
+        };
+        System.out.println(solution1(grid3, 0, 0)); // Expected: -1
+
+        // Test 4: start on a hotel
+        int[][] grid4 = {
+                {2, 0},
+                {0, 0}
+        };
+        System.out.println(solution1(grid4, 0, 0)); // Expected: 0
+    }
+
+    public static int solution1(int[][] grid, int startRow, int startCol) {
+        var coordinatesQueue = new ArrayDeque<int[]>();
+        coordinatesQueue.add(new int[]{startRow, startCol});
+
+        var visitedCoordinates = new boolean[grid.length][grid[0].length];
+        visitedCoordinates[startRow][startCol] = true;
+
+        int[][] directions = {
+                {-1, 0},    // up
+                {1, 0},     // down
+                {0, -1},    // left
+                {0, 1}      // right
+        };
+
+        var minimumNumberOfSteps = 0;
+
+        while (!coordinatesQueue.isEmpty()) {
+            var layerSize = coordinatesQueue.size();
+
+            for (var i = 0; i < layerSize; i++) {
+                var layer = coordinatesQueue.poll();
+                var row = layer[0];
+                var column = layer[1];
+
+                var currentPosition = grid[row][column];
+
+                if (currentPosition == 2) {
+                    return minimumNumberOfSteps;
+                }
+
+                for (var dir : directions) {
+                    int newRow = row + dir[0];
+                    int newCol = column + dir[1];
+
+                    if ((newRow >= 0 && newRow < grid.length) && (newCol >= 0 && newCol < grid[row].length) &&
+                            !visitedCoordinates[newRow][newCol] && grid[newRow][newCol] != 1) {
+
+                        visitedCoordinates[newRow][newCol] = true;
+                        var nextPosition = new int[]{newRow, newCol};
+
+                        coordinatesQueue.add(nextPosition);
+                    }
+
+                }
+            }
+            minimumNumberOfSteps++;
+        }
+
+        return -1;
+    }
+}
